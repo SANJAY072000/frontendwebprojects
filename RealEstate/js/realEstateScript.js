fe46e1665c2e4a36e0974bc583a6e5c9e3c0686f
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-05-12 22:19:27 +0530

    Commited projects successfully

---
+++ RealEstate/js/realEstateScript.js
@@ -0,0 +1,68 @@
+$(function(){
+    $(".bt").mouseover(function(){$(".bt").css("background","#E71C23").addClass("animated pulse shadow rounded");});
+    $(".bt").mouseout(function(){$(".bt").css({"background":"#3498DB"}).removeClass("animated pulse shadow rounded");});
+    $(".fa-phone").mouseover(function(){$(".fa-phone").css("color","#E71C23");});
+     $(".fa-phone").mouseout(function(){$(".fa-phone").css("color","#3498DB");});                           
+     $(".fa-envelope").mouseover(function(){$(".fa-envelope").css("color","#E71C23");});
+     $(".fa-envelope").mouseout(function(){$(".fa-envelope").css("color","#3498DB");});                            
+      $(".fa-clock-o").mouseover(function(){$(".fa-clock-o").css("color","#E71C23");});
+     $(".fa-clock-o").mouseout(function(){$(".fa-clock-o").css("color","#3498DB");});                           
+      $(".navbar-brand").mouseover(function(){$(".navbar-brand").addClass("animated tada");});
+     $(".navbar-brand").mouseout(function(){$(".navbar-brand").removeClass("animated tada");});                          
+    $(".cat .fa-circle-o-notch").mouseover(()=>{$(".fa-circle-o-notch").css({"color":"#E8290B","background":"white","border-radius":"30px"}); }); 
+    $(".cat .fa-circle-o-notch").mouseout(()=>{$(".fa-circle-o-notch").css({"color":"white","background":"#3498DB","border-radius":"0px"}); }); 
+    $(".cat .fa-hand-o-right").mouseover(()=>{$(".fa-hand-o-right").css({"color":"#E8290B","background":"white","border-radius":"30px"}); }); 
+    $(".cat .fa-hand-o-right").mouseout(()=>{$(".fa-hand-o-right").css({"color":"white","background":"#3498DB","border-radius":"0px"}); }); 
+    $(".cat .fa-cog").mouseover(()=>{$(".fa-cog").css({"color":"#E8290B","background":"white","border-radius":"30px"}); }); 
+    $(".cat .fa-cog").mouseout(()=>{$(".fa-cog").css({"color":"white","background":"#3498DB","border-radius":"0px"}); }); 
+    
+     $(".cat .fa-cube").mouseover(()=>{$(".fa-cube").css({"color":"#E8290B","background":"white","border-radius":"30px"}); }); 
+    $(".cat .fa-cube").mouseout(()=>{$(".fa-cube").css({"color":"white","background":"#3498DB","border-radius":"0px"}); }); 
+     $(".cat .fa-arrows-alt").mouseover(()=>{$(".fa-arrows-alt").css({"color":"#E8290B","background":"white","border-radius":"30px"}); }); 
+    $(".cat .fa-arrows-alt").mouseout(()=>{$(".fa-arrows-alt").css({"color":"white","background":"#3498DB","border-radius":"0px"}); }); 
+     $(".cat .fa-eye").mouseover(()=>{$(".fa-eye").css({"color":"#E8290B","background":"white","border-radius":"30px"}); }); 
+    $(".cat .fa-eye").mouseout(()=>{$(".fa-eye").css({"color":"white","background":"#3498DB","border-radius":"0px"}); }); 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+                                
+                                
+                                
+                                
+
+});
---
+++ Tribute/js/tributeScript.js
@@ -0,0 +1,9 @@
+var tribute = new Tribute({
+  values: [
+    {key: 'Katniss Everdeen', value: 'Kat_Catching_Fire'},
+    {key: 'Foxface', value: 'foxyweapons'},
+      {key: 'AmsterDam', value: 'Amsterdam'},
+      {key: 'Katn', value: 'fire now'}
+  ]
+})
+ tribute.attach(document.getElementById('arena'));
